77afa5d05ccc1619bb59922c91daab7a147024f6
Mahdi Parivash <mahdiparivash@protommail.com>
2022-08-22 19:20:13 +0430

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app-wrapper">
+    <div class="app">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
+}
+</style>
